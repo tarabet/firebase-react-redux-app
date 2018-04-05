e7dee8bb40669e13cbad17aca13dee0c91cd7398
Oleksandr Taranenko <ataranenko@evolutiongaming.com>
2018-04-05 10:51:07 +0300

    All async decrements applied. Ready to transfer app.

---
+++ src/containers/about/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+    <div>
+        <h1>About us</h1>
+        <p>Hello medium</p>
+    </div>
+);
---
+++ src/containers/home/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { push } from "react-router-redux";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import {
+    increment,
+    decrement,
+    incrementAsync,
+    decrementAsync,
+} from "../../modules/counter";
+
+const Home = props => (
+    <div>
+        <h1>Home</h1>
+        <p>Count: {props.count}</p>
+
+        <p>
+            <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+            <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+        </p>
+
+        <p>
+            <button onClick={props.decrement} disabled={props.isDecrementing}>Decrement</button>
+            <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+        </p>
+
+
+        <p>
+            <button onClick={() => props.changePage()} >Go to About page via Redux</button>
+        </p>
+    </div>
+);
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    increment,
+    incrementAsync,
+    decrement,
+    decrementAsync,
+    changePage: () => push("/about-us"),
+}, dispatch);
+
+const mapStateToProps = state => ({
+    count: state.counter.count,
+    isIncrementing: state.counter.isIncrementing,
+    isDecrementing: state.counter.isDecrementing,
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Home);
